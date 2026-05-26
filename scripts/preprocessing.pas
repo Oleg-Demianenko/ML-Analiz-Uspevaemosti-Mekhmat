6a47ae08d903cdb26639e79e8cf2bed1e4a10721
Александр Земляк <zemlyaksanya@mail.ru>
2026-05-26 21:05:53 +0300

    Add splitted lines in dataset fixing

--- scripts/preprocessing.pas
+++ scripts/preprocessing.pas
@@ -1,9 +1,15 @@
-﻿uses MLABC, DataFrameABCCore;
+﻿{$zerobasedstrings}
+uses MLABC, DataFrameABCCore, System.Text.RegularExpressions;
 
 const markColumnNames = ['экзамен', 'добор 1', 'добор 2', 'пересдача 1', 'пересдача 2', 'бонус'];
 
 begin
-  var df := DataFrame.FromCsv('..\data\УспеваемостьМехмат2017-2025.csv');
+  var dataText := ReadAllText('..\data\УспеваемостьМехмат2017-2025.csv', System.Text.Encoding.UTF8);
+  
+  // Исправляем строки, которые поделились на две
+  dataText := Regex.Replace(dataText, '\n",', '",');
+  
+  var df := DataFrame.FromCsvText(dataText);
   
   // Все колонки в LowerCase
   df.GetColumns.Select(col -> col.Info.Name)
@@ -11,7 +17,7 @@ begin
   
   // Удаляем строки со всеми пустыми оценками
   Println($'Кол-во записей до очистки по пустым оценкам: {df.RowCount()}');
-  df := df.Filter(r -> markColumnNames.Any(name -> r.Int(name) > 0));
+  df := df.Filter(r -> markColumnNames.Any(name -> r.IsValid(name)));
   Println($'Кол-во записей после очистки: {df.RowCount()}');
 
   
