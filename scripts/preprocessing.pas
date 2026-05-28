89145f12687681d9e8822e096304df373b7cc058
Oleg-Demianenko <92909188+Oleg-Demianenko@users.noreply.github.com>
2026-05-28 17:47:42 +0300

    Add total scores and deletion of disciplines without scores
        Added new column with student's total scores by discipline.
        Selected names of specialties of our faculty. They have much more records - beginning from several thousands, and the other faculties specialties have up to several hundreds of records.
        Selected names of disciplines which have zero total scores for all the students of selected specialties. And deleted records with them.

--- scripts/preprocessing.pas
+++ scripts/preprocessing.pas
@@ -2,6 +2,8 @@
 uses MLABC, DataFrameABCCore, System.Text.RegularExpressions;
 
 const markColumnNames = ['экзамен', 'добор 1', 'добор 2', 'пересдача 1', 'пересдача 2', 'бонус'];
+const examScoreColumns = ['экзамен', 'пересдача 1', 'пересдача 2'];
+const termScoreColumns = ['рейтинг в семестре', 'добор 1', 'добор 2', 'бонус'];
 
 begin
   var dataText := ReadAllText('..\data\УспеваемостьМехмат2017-2025.csv', System.Text.Encoding.UTF8);
@@ -12,15 +14,30 @@ begin
   var df := DataFrame.FromCsvText(dataText);
   
   // Все колонки в LowerCase
-  df.GetColumns.Select(col -> col.Info.Name)
-    .ForEach(colName -> begin df := df.Rename(colName, colName.ToLower()); end);
+  df.Schema.ColumnNames.ForEach(colName -> begin df := df.Rename(colName, colName.ToLower()); end);
   
   // Удаляем студентов без id
   df := df.Filter(row -> row.IsValid('id студента'));
   
-  // Удаляем строки со всеми пустыми оценками
+  // Добавляем столбец с общим баллом студента по дисциплине:
+  // сумма баллов за работу в семестре и доборы + максимальный балл за все попытки сдачи экзамена
+  df := df.WithColumnInt('общий балл', row -> termScoreColumns.Sum(name -> row.Int(name)) + 
+    examScoreColumns.Max(name -> row.Int(name)));
+    
+  // Выделим список специльностей Мехмата - они многократно отличаются
+  // количеством записей от специальностей других факультетов
+  var specNames := df.GroupBy('название специальности').Count
+  .Filter(row -> row.Int('count') > 1000).GetStrColumn('название специальности');
+  
+  // Выделим список дисциплин, по которым отсутствуют баллы у студентов Мехмата
+  var emptyScoreDisciplines := df.Filter(row -> specNames.Contains(row.Str('название специальности')))
+  .GroupBy('дисциплина').Sum('общий балл').Filter(row -> row.Float('общий балл_sum') = 0)
+  .GetStrColumn('дисциплина');
+  
+  // Удалим записи о тех дисциплинах, по которым отсутвуют баллы у студентов Мехмата
   Println($'Кол-во записей до очистки по пустым оценкам: {df.RowCount()}');
-  df := df.Filter(r -> markColumnNames.Any(name -> r.IsValid(name)));
+  df := df.Filter(row -> specNames.Contains(row.Str('название специальности')) and 
+  not emptyScoreDisciplines.Contains(row.Str('дисциплина')) or not specNames.Contains(row.Str('название специальности')));
   Println($'Кол-во записей после очистки: {df.RowCount()}');
 
 end.
